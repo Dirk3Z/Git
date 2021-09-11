572e49df5556a80959ce7db4569a0fd033e325cf
3Zz <1193643424@qq.com>
2021-09-11 15:52:09 +0800

    push commit2

--- src/main/java/com/atguigu/git/FitTest.java
+++ src/main/java/com/atguigu/git/FitTest.java
@@ -13,5 +13,6 @@ public class FitTest {
         System.out.println("hello    git4！！");
         System.out.println("master  test！！");
         System.out.println("hot-fix test！！");
+        System.out.println("push test！！");
     }
 }

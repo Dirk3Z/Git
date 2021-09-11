5f16b2a4045c366c1aeef849aca53246d1ca8b1d
3Zz <1193643424@qq.com>
2021-09-11 10:49:25 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/FitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Auther: zzz
+ * @Date: 2021/9/11 10:38
+ * @Description:
+ */
+public class FitTest {
+    public static void main(String[] args){
+        System.out.println("hello  Git!!!");
+    }
+}
